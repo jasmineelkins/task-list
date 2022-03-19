40a69730906394bc23d31f4f376646fe02f66226
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-03-18 21:55:26 -0400

    Initial commit: created js, html, css, json files and got dev server working

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Task List</h1>
+      <ul>List items go here:</ul>
+    </>
+  );
+}
+
+export default App;
